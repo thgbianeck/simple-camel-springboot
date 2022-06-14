ece00eb07075c1c65796d822ea785f4036b1796d
Thiago Moreira Bianeck <thgbianeck@gmail.com>
2022-06-14 15:23:05 -0300

    Initial commit

---
+++ src/main/java/br/com/bnck/SimpleCamelSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.bnck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCamelSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCamelSpringbootApplication.class, args);
+    }
+
+}
