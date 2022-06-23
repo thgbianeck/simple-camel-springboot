bee46113f38481120f40f373caab78b18eaef5c6
Thiago Moreira Bianeck <thgbianeck@gmail.com>
2022-06-23 10:53:30 -0300

    Seção 2: Simple Apache Camel Route and JUnit

---
+++ src/main/java/br/com/bnck/components/LegacyFileRoute.java
@@ -0,0 +1,16 @@
+package br.com.bnck.components;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LegacyFileRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+        from("file:src/data/input?fileName=inputFile.txt")
+                .routeId("legacyFileMoveRouteId")
+                .to("file:src/data/output?fileName=outputFile.txt");
+
+    }
+}
