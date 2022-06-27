bee7d9855dfbc449c02ed54d5bf8dc5df0b859ac
Thiago Moreira Bianeck <thgbianeck@gmail.com>
2022-06-27 01:07:35 -0300

    PDI-22 - Curso concluído

--- src/main/java/br/com/bnck/components/NewRestRoute.java
+++ src/main/java/br/com/bnck/components/NewRestRoute.java
@@ -2,16 +2,25 @@ package br.com.bnck.components;
 
 import br.com.bnck.beans.NameAddress;
 import br.com.bnck.processor.InboundMessageProcessor;
+import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import java.net.ConnectException;
 
 @Component
 public class NewRestRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+
+        onException(JMSException.class, ConnectException.class)
+                .routeId("jmsExceptionRouteId")
+                .handled(true)
+                .log(LoggingLevel.INFO, "JMS Exception has occurred; handling gracefully");
 
         restConfiguration()
                 .component("jetty")
@@ -34,7 +43,25 @@ public class NewRestRoute extends RouteBuilder {
 //                .log(LoggingLevel.INFO, "Transformed Body: ${body}")
 //                .convertBodyTo(String.class)
 //                .to("file:src/data/output?fileName=outputFile.csv&fileExist=append&appendChars=\\n");
+                .log(LoggingLevel.INFO, ">>> Sending to DB EndPoint")
+                .to("direct:toDB")
+                .log(LoggingLevel.INFO, ">>> Sending to AMQ EndPoint")
+                .to("direct:toActiveMQ")
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(200))
+                .transform().simple("Message Processed and Result Generated with Body: ${body}")
+                .end();
+        ;
+
+        from("direct:toDB")
+                .routeId("toDBId")
+                .log(LoggingLevel.INFO, ">>> In DB EndPoint")
+                .to("jpa:" + NameAddress.class.getName())
+        ;
+
+        from("direct:toActiveMQ")
+                .routeId("toActiveMQId")
+                .log(LoggingLevel.INFO, ">>> In AMQ EndPoint")
                 .to("activemq:queue:nameaddressqueue?exchangePattern=InOnly")
-                .to("jpa:" + NameAddress.class.getName());
+        ;
     }
 }
